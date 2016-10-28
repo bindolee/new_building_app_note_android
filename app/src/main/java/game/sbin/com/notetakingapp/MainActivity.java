66ca964d25c504e394ab00d3b866b1182bb0b993
bindolee <bindolee@gmail.com>
2016-10-27 23:16:13 -0700

    Implemented Loader/Loadermanger to handle task asynchronously.. for better efficiency.

--- app/src/main/java/game/sbin/com/notetakingapp/MainActivity.java
+++ app/src/main/java/game/sbin/com/notetakingapp/MainActivity.java
@@ -1,22 +1,29 @@
 package game.sbin.com.notetakingapp;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.widget.CursorAdapter;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor>
+{
+
+    private CursorAdapter cursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
         insertNote("New Note");
 
-        //Cursor object to access all of the data content.
-        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI,
-                DBOpenHelper.ALL_COLUMNS, null, null, null, null);
         String[] from = {DBOpenHelper.NOTE_TEXT};
         int[] to = {android.R.id.text1};
-        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1, cursor, from, to, 0);
+
+        cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1, null, from, to, 0);
 
         ListView lv = (ListView) findViewById(android.R.id.list);
         lv.setAdapter(cursorAdapter);
+        getLoaderManager().initLoader(0,null, this);
     }
 
     private void insertNote(String noteText) {
@@ -76,5 +82,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this, NotesProvider.CONTENT_URI,
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
     }
 }
